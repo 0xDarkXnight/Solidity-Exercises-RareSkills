ba4a10bb74e1543c8e41b204b9e00c7f590b086c
0xDarkXnight <akshat_g1@me.iitr.ac.in>
2025-10-02 13:53:15 +0530

    Withdraw Done

--- Withdraw/src/Withdraw.sol
+++ Withdraw/src/Withdraw.sol
@@ -3,7 +3,13 @@ pragma solidity ^0.8.13;
 
 contract Withdraw {
     // @notice make this contract able to receive ether from anyone and anyone can call withdraw below to withdraw all ether from it
-    function withdraw() public {
+    function withdraw() public returns(bool) {
         // your code here
+        (bool ok, ) = msg.sender.call{value: address(this).balance}("");
+        return ok;
+    }
+
+    receive() external payable {
+        
     }
 }
